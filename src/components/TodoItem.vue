19efecbf7e873d2cf223d6e6c6bba2be2e9db5c2
Artur Cynk <arturcynk197@gmail.com>
2024-06-04 21:29:01 +0200

    app

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="todo-item">
+        <span>{{todo.text}}</span>
+        <butoon @click="$emit('delete-todo',todo.id)">Delete</butoon>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        todo: Object
+    }
+}
+</script>
+
+<style scoped>
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  background: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background: darkred;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @delete-todo="deleteTodo" />
+        <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a todo" />
+    </div>
+</template>
+
+<script>
+    import TodoItem from './TodoItem.vue';
+
+    export default {
+        name: 'TodoList',
+        components: {
+            TodoItem
+        },
+
+        data() {
+            return {
+                newTodo: '',
+                todos: [
+                    {id: 1, text: 'Learn Vue.js'},
+                    {id: 2, text: 'Build a Todo App'}
+                ]
+            }
+        },
+        methods: {
+            addTodo(){
+                if(this.newTodo.trim()){
+                    this.todos.push({id:this.todos.length + 1, text: this.newTodo}),
+                    this.newTodo = '';
+                }
+            },
+            deleteTodo(id){
+                this.todos = this.todos.filter(todo => todo.id !== id);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    input{
+        margin-top: 20px;
+        padding: 10px;
+        font-size: 16px;
+    }
+</style>
